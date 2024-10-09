43e1eeccc42b5bf05e8f70daf699e1794999b60d
Nathan Shturm <exterminathan@gmail.com>
2024-10-09 11:56:57 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,70 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Sheep Counter";
 document.title = gameName;
 
+// Important Values
+let ct = 0;
+let defGrowRate = 1;
+
+
 // Title Card
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
 // Sheep Button
-const emoji = "🐑";
-const button = document.createElement("button");
+const sheep_emoji = "🐑";
+const sheep_button = document.createElement("button");
 
-button.innerHTML = emoji;
-app.append(button);
+sheep_button.innerHTML = sheep_emoji;
+app.append(sheep_button);
+
+
+// Upgrade button
+const upgrade_emoji = "⬆️";
+const upgrade_button = document.createElement("button");
+
+upgrade_button.innerHTML = upgrade_emoji;
+upgrade_button.disabled = true;
+
+app.append(upgrade_button);
+
+
 
 // Sheep counter
 const count_display = document.createElement("div");
-let ct = 0;
 
 count_display.innerHTML = `${ct.toFixed(1)} Sheep Counted`;
 app.append(count_display);
 
 // Sheep button listener
-button.addEventListener("click", () => {
+sheep_button.addEventListener("click", () => {
   ct++;
   count_display.innerHTML = `${ct.toFixed(1)} Sheep Counted`;
+  checkUpgrade();
 });
 
-// Automatic clicking - Previous Version
-// setInterval(() => {
-//   ct++;
-//   count_display.innerHTML = `${ct} Sheep Counted`;
-// }, 1000);
+
+// lil bit of Extract Function (106) happening here
+function checkUpgrade() {
+  if (ct >= 10) {
+    upgrade_button.disabled = false;
+  } else {
+    upgrade_button.disabled = true;
+  }
+}
+
+
+// Upgrade button listener
+upgrade_button.addEventListener("click", () => {
+  if (ct >= 10) {
+    ct -= 10;
+    defGrowRate +=1;
+    count_display.innerHTML = `${ct.toFixed(1)} Sheep Counted`;
+    checkUpgrade();
+  }
+})
+
+
 
 // Continuous Growth
 let lastFrameT = performance.now();
@@ -44,8 +78,10 @@ function updateCounter(time: number) {
   const deltaT = (time - lastFrameT) / 1000;
   lastFrameT = time;
 
-  ct += deltaT;
+  ct += defGrowRate * deltaT;
   count_display.innerHTML = `${ct.toFixed(1)} Sheep Counted`;
+
+  checkUpgrade();
 
   requestAnimationFrame(updateCounter);
 }

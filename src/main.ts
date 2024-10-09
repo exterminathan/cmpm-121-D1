d549d7118b220b64bdff366c1cbd8e5c78eb52e2
Nathan Shturm <exterminathan@gmail.com>
2024-10-09 11:34:55 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,34 +5,49 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Sheep Counter";
 document.title = gameName;
 
-//Title Card
+// Title Card
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-//Sheep Button
+// Sheep Button
 const emoji = "🐑";
 const button = document.createElement("button");
 
 button.innerHTML = emoji;
 app.append(button);
 
-//Sheep counter
+// Sheep counter
 const count_display = document.createElement("div");
 let ct = 0;
 
-count_display.innerHTML = `${ct} Sheep Counted`;
+count_display.innerHTML = `${ct.toFixed(1)} Sheep Counted`;
 app.append(count_display);
 
-//Sheep button listener
+// Sheep button listener
 button.addEventListener("click", () => {
   ct++;
-  count_display.innerHTML = `${ct} Sheep Counted`;
+  count_display.innerHTML = `${ct.toFixed(1)} Sheep Counted`;
 });
 
+// Automatic clicking - Previous Version
+// setInterval(() => {
+//   ct++;
+//   count_display.innerHTML = `${ct} Sheep Counted`;
+// }, 1000);
 
-//Automatic clicking
-setInterval(() => {
-    ct++;
-    count_display.innerHTML = `${ct} Sheep Counted`;
-}, 1000);
+// Continuous Growth
+let lastFrameT = performance.now();
+
+function updateCounter(time: number) {
+  // time diff between frames
+  const deltaT = (time - lastFrameT) / 1000;
+  lastFrameT = time;
+
+  ct += deltaT;
+  count_display.innerHTML = `${ct.toFixed(1)} Sheep Counted`;
+
+  requestAnimationFrame(updateCounter);
+}
+
+requestAnimationFrame(updateCounter);

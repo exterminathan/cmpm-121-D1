b249f31a3ff44cf075187acf9473466a27d0a0c9
Nathan Shturm <exterminathan@gmail.com>
2024-10-08 22:24:57 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My awesome game";
+const gameName = "Sheep Counter";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const emoji = "🐑";
+const button = document.createElement("button");
+
+button.innerHTML = emoji;
+app.append(button);
